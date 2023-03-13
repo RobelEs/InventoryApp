cafc14172076fcce0a0c589d6aa260a5244b04ba
RobelEs <robel.abrh@eduge.ch>
2023-03-13 14:05:18 +0100

    First commit

---
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.inventory
+
+import android.content.ClipData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.inventory.data.ItemDao
+import kotlinx.coroutines.launch
+import androidx.lifecycle.viewModelScope
+
+
+class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
+
+
+}
+
+class InventoryViewModelFactory(private val itemDao: ItemDao) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        TODO("Not yet implemented")
+    }
+}
